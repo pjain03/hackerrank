269bd0d4a502d54f4775578ab60009d23cbbb608
pjain03 <pulkit.jain@tufts.edu>
2018-01-11 22:42:17 +0530

    what the idea looks like

--- Travel Around the World/solution.py
+++ Travel Around the World/solution.py
@@ -1,23 +1,37 @@
 N, C = [int(x) for x in input().split()]
 a = [int(x) for x in input().split()]
 b = [int(x) for x in input().split()]
+valid_city = [True for x in range(N)]
 
-# first shot at storing previous info for dyn. prog. Don't see how this would reduce computation.
-diff = [a[x] - b[x] for x in range(N)]
-num = N
-# start at a city
 for i in range(N):
-    gas = min(C, a[i])
-    k = i
-    # travel to other cities
-    for j in range(N):
-        # unless no gas left for travel
-        gas -= b[k]    
-        if gas < 0:
-            num -= 1
-            break
-        k = (k + 1) % N
-        # fill as much as possible
-        gas = min(C, gas + a[k])
-        
-print(num)        
+    if b[i] > C or b[i] - a[i] > C:
+        print(0)
+        exit()   
+    a[i] = min(a[i], C)    
+
+# so that starting at any point in this (in first N), we take N steps and reach the start again
+a = a + a
+b = b + b
+for i in range(N):
+    if valid_city:
+        # you can travel from this city to next if a[i] > b[i]
+        diff = a[i] - b[i]
+        # if not ie a[i] < b[i] meaning it costs more to travel than we have
+        if diff < 0:
+            valid_city[i] = False
+            # max value this can take is 2N by design, so we only need to be cautious of staying in bounds of valid_city
+            k = i + 1
+            change = 0
+            while diff < 0 and change < N:
+                diff += a[k] - b[k]
+                if diff < 0:
+                    valid_city[k % N] = False
+                change += 1
+                k += 1
+
+sum_valid_cities = 0
+for i in range(N):
+    if valid_city[i]:
+        print(i)
+        sum_valid_cities += 1
+print(sum_valid_cities)        
